a04d48e53d408f7e1d8ff96d6efa6ef3b83c5fac
Gem Ramirez <geraldinemaesramirez@gmail.com>
2024-02-23 06:57:07 +0800

    TeamB CourseEntity Updated

--- integration/src/main/java/com/backend/integration/Entity/Course.java
+++ integration/src/main/java/com/backend/integration/Entity/Course.java
@@ -1,5 +1,6 @@
 package com.backend.integration.Entity; // Package declaration
 
+
 import java.sql.Date;
 import java.util.List;
 
@@ -11,8 +12,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
 @Entity
